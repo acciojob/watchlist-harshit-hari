44f89418a715d2310bfc7d4e840508e6876fc017
Unreserved-Bihari <rs5168732@gmail.com>
2022-12-03 03:27:31 +0530

    Completed

--- src/main/java/com/driver/Movie.java
+++ src/main/java/com/driver/Movie.java
@@ -1,2 +1,42 @@
-package com.driver;public class Movie {
+package com.driver;
+
+public class Movie {
+    private String name;
+    private int durationInMinutes;
+    private double imbdRating;
+
+
+    Movie() {
+    }
+
+
+    public Movie(String name, int durationInMinutes, double imbdRating) {
+        this.name = name;
+        this.durationInMinutes = durationInMinutes;
+        this.imbdRating = imbdRating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDurationInMinutes() {
+        return durationInMinutes;
+    }
+
+    public void setDurationInMinutes(int durationInMinutes) {
+        this.durationInMinutes = durationInMinutes;
+    }
+
+    public double getImbdRating() {
+        return imbdRating;
+    }
+
+    public void setImbdRating(double imbdRating) {
+        this.imbdRating = imbdRating;
+    }
 }
--- src/main/java/com/driver/MovieService.java
+++ src/main/java/com/driver/MovieService.java
@@ -1,2 +1,52 @@
-package com.driver;public class MovieService {
+package com.driver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MovieService {
+
+@Autowired
+MovieRepository movieRepository;
+
+    public void addMovie(Movie movie) {
+        movieRepository.saveMovie(movie);
+    }
+    public void  addDirector(Director director){
+        movieRepository.saveDirector(director);
+    }
+
+    public void addMovieDirectorPair(String movie , String director){
+        movieRepository.saveMovieDirectorPair(movie , director);
+
+    }
+    public Movie  getMovieByName(String movieName){
+       return movieRepository.getMovieByName(movieName);
+    }
+
+    public Director  getDirectorByName(String name){
+        return movieRepository.getDirectorByName(name);
+
+    }
+
+    public List<String> getMoviesByDirectorName(String name){
+       return movieRepository.getMoviesByDirectorName(name);
+    }
+    public List<String> findAllMovies(){
+        return movieRepository.findAllMovies();
+    }
+
+    public void deleteDirector(String director){
+        movieRepository.deleteDirector(director);
+    }
+
+    public void deleteAllDirectors(){
+        movieRepository.deleteAllDirector();
+    }
+
+
+
 }
